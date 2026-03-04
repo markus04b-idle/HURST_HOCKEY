6b5307f2579054792a8db1ba10e1384da6564481
Markus Brown <markus04b@gmail.com>
2026-03-04 15:49:02 +0000

    different changes

--- read.py
+++ read.py
@@ -1,11 +1,14 @@
 from sqlmodel import Session, select
 from models import engine, Bio, Stats
 import pandas as pd
+from sqlalchemy import func
 
 with Session(engine) as session:
+
+
     statement = (
-        select(Bio.first_name, Bio.last_name, Bio.position)
-        .order_by(Bio.position, Bio.last_name)
+        select(Bio.position, Bio.last_name)
+        .group_by(Bio.position, Bio.last_name)
     )
     records = session.exec(statement).all()
     
